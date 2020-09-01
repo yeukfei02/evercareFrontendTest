0a10e86470d1e9211eb65def1791d7ce520c39e7
Donald Wu <yeukfei02@gmail.com>
2020-09-01 18:50:41 +0800

    refactor: 💡 add get users request

--- pages/mainView.tsx
+++ pages/mainView.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+interface Users {
+  id: string;
+  lastName: string;
+  firstName: string;
+  email: string;
+  title: string;
+  picture: string;
+}
 
 function MainView(): JSX.Element {
+  const [usersList, setUsersList] = useState<Users[]>([]);
+
+  useEffect(() => {
+    getUsersList();
+  }, []);
+
+  const getUsersList = async () => {
+    const response = await fetch('/api/users');
+    if (response) {
+      const responseJSON = await response.json();
+      console.log('responseJSON = ', responseJSON);
+
+      if (responseJSON.result && responseJSON.result.users) {
+        setUsersList(responseJSON.result.users);
+      }
+    }
+  };
+
   return (
     <div>
       <h1>MainView</h1>
